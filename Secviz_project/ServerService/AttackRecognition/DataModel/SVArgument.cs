84e4c392ce717adf17312a45c56c4c3eed063cb9
svic4ever <svic4ever@3fa042e2-020d-5fbe-4809-865d902d7e88>
2010-11-09 07:47:25 +0000

    ADD trunk/Secviz_project/ServerService/AttackRecognition/DataModel/SVArgument.cs Argument Class

---
+++ Secviz_project/ServerService/AttackRecognition/DataModel/SVArgument.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServerService.AttackRecognition.DataModel
+{
+    public class Argument
+    {
+        private int ID;
+        private String Name;
+        private String Type;
+
+        public Argument()
+        {
+
+        }
+
+        public Argument(int ID, String Type, String Name)
+        {
+            this.ID = ID;
+            this.Type = Type;
+            this.Name = Name;
+        }
+    }
+}
