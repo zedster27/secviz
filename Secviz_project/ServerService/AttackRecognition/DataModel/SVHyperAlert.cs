2ba692eac837ca0a320f3f7c91655c6b740a17ad
lequocthang45 <lequocthang45@gmail.com>
2010-11-10 04:04:13 +0000

    ADD trunk/Secviz_project/ServerService/AttackRecognition/DataModel/SVHyperAlert.cs HyperAlert's class ADD trunk/Secviz_project/ServerService/AttackRecognition/DataModel/SVImplication.cs Implication's class

---
+++ Secviz_project/ServerService/AttackRecognition/DataModel/SVHyperAlert.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServerService.AttackRecognition.DataModel
+{
+    public class HyperAlert
+    {
+        private DateTime beginTime;
+        private int[] consequenceSet;
+        private DateTime endTime;
+        private String hyperAlertType;
+        private int ID;
+        private int[] prerequisiteSet;
+
+        public HyperAlert()
+        { 
+
+        }
+    }
+}
---
+++ Secviz_project/ServerService/AttackRecognition/DataModel/SVImplication.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServerService.AttackRecognition.DataModel
+{
+    public class Implication
+    {
+        private int[] impliedArgIDArray;
+        private String impliedName;
+        private int[] implyingArgIDArray;
+        private String implyingName;
+
+        public int getImpliedArgID(int index)
+        {
+            return 0;
+        }
+
+        public int getImplyingArgID(int index)
+        {
+            return 0;
+        }
+
+        public Implication()
+        { 
+
+        }
+
+        public Implication(String implying, String implied)
+        {
+            this.impliedName = implied;
+            this.implyingName = implying;
+        }
+    }
+}
