95be34d565db659a1b9b6d248cd61e18fece33ae
kimyumiko <kimyumiko@3fa042e2-020d-5fbe-4809-865d902d7e88>
2011-01-29 14:35:05 +0000

    ADD /branches/Kim/KnowledgeBaseCreation/DatabaseConnection.cs

---
+++ branches/Kim/KnowledgeBaseCreation/KnowledgeBaseCreation/DatabaseConnection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+
+namespace KnowledgeBaseCreation
+{
+    class DatabaseConnection
+    {
+        public static OracleConnection getDatabaseConnection(string host, string port, string databaseName, string username, string password)
+        {
+            string connString = "Data Source=" + host + ":" + port + "/" + databaseName + ";"
+                                + "User Id=" + username + ";" + "Password=" + password + ";";
+            OracleConnection conn = new OracleConnection(connString);
+            return conn;
+        }
+
+        public static OracleConnection getDatabaseConnection(string connString)
+        {
+            OracleConnection conn = new OracleConnection(connString);
+            return conn;
+        }
+    }
+}
