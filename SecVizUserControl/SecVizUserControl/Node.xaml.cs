f93d46815721c4999a0d59f2f31d69aa98075490
kimyumiko <kimyumiko@3fa042e2-020d-5fbe-4809-865d902d7e88>
2011-03-03 08:54:04 +0000

    ADD project SecVizUserControl

---
+++ SecVizUserControl/SecVizUserControl/Node.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SecVizUserControl
+{
+    /// <summary>
+    /// Interaction logic for Node.xaml
+    /// </summary>
+    public partial class Node : UserControl
+    {
+        public Node()
+        {
+            InitializeComponent();                 
+        }
+
+        public Node(double width, double height, string hyperAlertType, string hyperAlertName, DateTime beginTime, DateTime endTime)
+        {
+            InitializeComponent();
+            this.Width = width;
+            this.Height = height;
+            this.HyperAlertType = hyperAlertType;
+            this.HyperAlertName = hyperAlertName;
+            hyperAlertName_textBlock.Text = hyperAlertName;
+            hyperAlertName_textBlock.Width = width;
+            this.BeginTime = beginTime;
+            this.EndTime = endTime;
+        }     
+        public string HyperAlertType;
+        public string HyperAlertName;
+        public DateTime BeginTime;
+        public DateTime EndTime;
+    }
+}
---
+++ SecVizUserControl/SecVizUserControl/TabItem.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SecVizUserControl
+{
+    /// <summary>
+    /// Interaction logic for TabItem.xaml
+    /// </summary>
+    public partial class TabItem : UserControl
+    {
+        public TabItem(string nameOfMainButton)
+        {
+            InitializeComponent();
+            IsSelected = false;
+            this.BorderBrush = Brushes.Blue;
+
+            this.Width = BUTTON_WIDTH + 30;
+            this.Height = BUTTON_HEIGHT;
+            main_button.Width = BUTTON_WIDTH;
+            main_button.Height = BUTTON_HEIGHT;
+            main_button.Content = nameOfMainButton;
+            buttonList = new List<Button>();
+        }
+        public void AddButton(string name)
+        {
+            Button newButton = new Button();
+            newButton.Width = BUTTON_WIDTH;
+            newButton.Height = BUTTON_HEIGHT;
+            newButton.Content = name;
+            newButton.Margin = new Thickness(30, 
+                                             (NumOfButton+1) * BUTTON_HEIGHT, 
+                                             BUTTON_WIDTH + 30,
+                                             (NumOfButton + 2) * BUTTON_HEIGHT);
+            newButton.Visibility = Visibility.Collapsed;
+
+            buttonList.Add(newButton);
+
+            this.Height += BUTTON_HEIGHT;
+            mainItemGrid.Children.Add(newButton);           
+
+            NumOfButton++;
+        }
+        private List<Button> buttonList = null;
+        public int SelectedButton;
+        public int NumOfButton = 0;
+        public bool IsSelected;
+        private const double BUTTON_WIDTH = 120;
+        private const double BUTTON_HEIGHT = 30;
+
+        private void main_button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
